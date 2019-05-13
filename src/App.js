278cebec0117f70cd817bbb1f3f6081575d65059
StephenReiser <Stephen.Reiser1@gmail.com>
2019-05-13 15:50:41 -0400

    fixed typo for heroku link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ let baseURL = ''
 if (process.env.NODE_ENV === 'development') {
   baseURL = 'http://localhost:3003'
 } else {
-  baseURL = 'https://quiet-garden-36890.herokuapp.com/'
+  baseURL = 'https://quiet-garden-36890.herokuapp.com'
 }
 
 class App extends React.Component {
