5ece08c9fb8b5ed01416b387e918f614e98a8a55
main_user <main_user@main-users-MacBook-Pro.local>
2019-05-14 11:45:12 -0400

    can consolelog data from quandl api

--- src/components/futures.js
+++ src/components/futures.js
@@ -2,16 +2,44 @@ import React from "react";
 var Chart = require("chart.js");
 
 class ChartFutures extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+    constructor(props) {
+        super(props)
+        this.state = {
+            baseURL: 'https://www.quandl.com/api/v3/datasets/ODA/',
+            futuresTitle: '',
+            query: '?',
+            apikey: 'api_key=' + 'zqzws_Cdwyy_r6nHyFCT',
+            searchURL: '',
+            // https://www.quandl.com/api/v3/datasets/ODA/PORANG_USD?api_key=zqzws_Cdwyy_r6nHyFCT
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({ [event.target.id]: event.target.value })
+    }
+    handleSubmit(event) {
+        event.preventDefault()
+        this.setState({
+            searchURL: this.state.baseURL + this.state.futuresTitle + this.state.query + this.state.apikey
+        }, () => {
+            console.log(this.state.searchURL)
+            fetch(this.state.searchURL)
+                .then(response => {
+                    return response.json()
+                }).then(json => console.log(json),
+                    err => console.log(err))
+        })
+    }
+
     componentDidMount() {
         const node = this.node;
 
         var myChart = new Chart(node, {
             type: 'doughnut',
             data: {
-                labels: ['Green','Red'],
+                labels: ['Green', 'Red'],
                 // ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
                 datasets: [{
                     label: 'Good Until',
@@ -52,10 +80,25 @@ class ChartFutures extends React.Component {
     render() {
         return (
             <div>
+                <React.Fragment>
+                    <form onSubmit={this.handleSubmit}>
+                        <label htmlFor='futuresTitle'>Title</label>
+                        <input
+                            id='futuresTitle'
+                            type='text'
+                            value={this.state.futuresTitle}
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            type='submit'
+                            value='Find commodity Info'
+                        />
+                    </form>
+
+                </React.Fragment>
                 <div className="row">
                     <div className="col-3"></div>
-                    <div className="col-6">
-\                        <canvas
+                    <div className="col-6">                    <canvas
                             style={{ width: 100, height: 50 }}
                             ref={node => (this.node = node)}
                         />
